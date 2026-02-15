22c635ad2b6b72c715090c82360925c6885a0917
Divyanshu <divyanshusingh4755@gmail.com>
2026-02-15 13:14:26 +0530

    added patch in cors

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(helmet())
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }))
 app.use(express.json())
